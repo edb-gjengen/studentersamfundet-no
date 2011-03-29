1b55b17bc330fc9193a1257c0568a9d00a73a300
Nikolai Kristiansen <nikolaik@gmail.com>
2011-03-29 20:17:11 +0200

    added function lol()

---
+++ wp-content/themes/neuf/functions.php
@@ -0,0 +1,9 @@
+/* loaded right before our parent's theme f́unctions.php file */
+
+/* Note: If including other files from here use STYLESHEETPATH like below.
+ * require_once( STYLESHEETPATH . '/path/to/file/in/child/theme.php' );
+ */
+
+function lol() {
+    echo '<h1>LOL</h1>\n';
+}
