0890a3160c4e610d5ac2f96586c160b6414efcff
Nikolai Kristiansen <nikolaik@gmail.com>
2011-03-29 19:39:07 +0200

    forgot to add files

---
+++ wp-content/themes/neuf/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title><?php  ?> - <?php bloginfo('name');?></title>
+
+<?php
+wp_head();
+?>
+
+</head>
+<body>
+
+<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
