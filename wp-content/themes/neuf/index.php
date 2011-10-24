8d0ac75251097d3b4f89429d5367d82a02ebc212
Nikolai Kristiansen <nikolaik@gmail.com>
2011-10-24 23:13:28 +0200

    Puts events on frontpage.

--- wp-content/themes/neuf/index.php
+++ wp-content/themes/neuf/index.php
@@ -1,14 +1,26 @@
 <?php
 get_header();
+$args = array(
+      'post_type' => 'event',
+      'posts_per_page' => -1,
+      'meta_key' => 'neuf_events_starttime',
+      'orderby' => 'meta_value',
+      'order' => 'ASC'
+      );
 
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-      the_content();
-   endwhile;
+$loop = new WP_Query( $args );
+
+if ($loop->have_posts()) :
+    while ($loop->have_posts()) : $loop->the_post();
+        the_title();
+        echo '<div class="entry-content">';
+        the_content();
+        echo '</div>';
+    endwhile;
+else:
+    echo "No events";
 endif;
 
-lol();
 get_sidebar();
 get_footer(); 
 ?>
