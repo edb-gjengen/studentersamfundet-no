a6fdea0ad39b2f62203d01dd402c58bd7c27a569
Arne Hassel <megoth@arne-ubuntu.(none)>
2011-10-25 15:38:48 +0200

    lagt til neuf-web i themes; lages med compass-rammeverket compass-style.org

---
+++ wp-content/themes/neuf-web/footer.php
@@ -0,0 +1,40 @@
+
+	<div id="root_footer"></div>
+</div>
+<div id="footer">
+	<?php wp_footer(); ?>
+	<ul id="supporters">
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/uio.png">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/studio.png">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/logo_sh86.png">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/toro.png">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/akademika.png">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/nrf.png">
+			</a>
+		</li>
+	</ul>
+</footer>
+</body>
+</html>
