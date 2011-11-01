6b22a63017c8ddab9418b2f75090499b1dfad3d6
Arne Hassel <megoth@arne-ubuntu.(none)>
2011-11-01 20:02:33 +0100

    Implementert litt responsive design, pluss begynt på innholdet

--- wp-content/themes/neuf-web/index.php
+++ wp-content/themes/neuf-web/index.php
@@ -10,41 +10,43 @@ $args = array(
       );
 ?>
 <article role="main_content">
-<?php
-$loop = new WP_Query( $args );
+	<div role="main_content_inner">
+		<?php
+		$loop = new WP_Query( $args );
 
-if ($loop->have_posts()) :
-	  while ($loop->have_posts()) : $loop->the_post();
-	      ?>
-            <div class="event">
-                <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail('event-image'); ?><br /><?php the_title(); ?></a>
-                <div class="event datetime"><?php
-                    echo date_i18n(get_option('date_format') . " k\l. " .get_option('time_format'), get_post_meta(get_the_ID(), 'neuf_events_starttime',true) );
-                ?></div>
-                <div class="event price"><?php
-                    $price = get_post_meta(get_the_ID(), 'neuf_events_price',true); 
-                    echo ($price != "" ? $price : "Gratis");
-                    ?></div>
-            </div>
-	      <?php
-	  endwhile;
-else:
-	  echo "No events";
-endif;
+		if ($loop->have_posts()) :
+				while ($loop->have_posts()) : $loop->the_post();
+					  ?>
+				        <div class="event">
+				            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail('event-image'); ?><br /><?php the_title(); ?></a>
+				            <div class="event datetime"><?php
+				                echo date_i18n(get_option('date_format') . " k\l. " .get_option('time_format'), get_post_meta(get_the_ID(), 'neuf_events_starttime',true) );
+				            ?></div>
+				            <div class="event price"><?php
+				                $price = get_post_meta(get_the_ID(), 'neuf_events_price',true); 
+				                echo ($price != "" ? $price : "Gratis");
+				                ?></div>
+				        </div>
+					  <?php
+				endwhile;
+		else:
+				echo "No events";
+		endif;
 
-echo '<div id="posts">';
-if (have_posts()) :
-	 while (have_posts()) :
-                  echo '<div class="post"';
-	    the_post();
-	    the_content();
-                  echo '</div>';
-	 endwhile;
-endif;
-echo '</div>';
-?>
+		echo '<div id="posts">';
+		if (have_posts()) :
+			 while (have_posts()) :
+				              echo '<div class="post"';
+					the_post();
+					the_content();
+				              echo '</div>';
+			 endwhile;
+		endif;
+		echo '</div>';
+		?>
+	</div>
 </article>
-<aside>
+<aside role="support_main_content">
 <?php
 get_sidebar();
 ?>
