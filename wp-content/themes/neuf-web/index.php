0c7ee0c14e05c0507b11c0c57bb3fb1de1c7135f
Just Thomas Misund <t@studentersamfundet.no>
2011-11-02 22:42:17 +0100

    Some semantics; changing an article to section and so forth

--- wp-content/themes/neuf-web/index.php
+++ wp-content/themes/neuf-web/index.php
@@ -9,15 +9,17 @@ $args = array(
       'order' => 'ASC'
       );
 ?>
-<article role="main_content">
-	<div role="main_content_inner">
+<section role="main">
 		<?php
 		$loop = new WP_Query( $args );
 
 		if ($loop->have_posts()) :
+?>
+				<div id="events" class="hfeed">
+<?php
 				while ($loop->have_posts()) : $loop->the_post();
 					  ?>
-				        <div class="event">
+				        <article class="event">
 				            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail('event-image'); ?><br /><?php the_title(); ?></a>
 				            <div class="event datetime"><?php
 				                echo date_i18n(get_option('date_format') . " k\l. " .get_option('time_format'), get_post_meta(get_the_ID(), '_neuf_events_starttime',true) );
@@ -26,27 +28,29 @@ $args = array(
 				                $price = get_post_meta(get_the_ID(), '_neuf_events_price',true); 
 				                echo ($price != "" ? $price : "Gratis");
 				                ?></div>
-				        </div>
+				        </article> <!-- .event -->
 					  <?php
 				endwhile;
+?>
+				</div> <!-- #events.hfeed -->
+<?php	
 		else:
 				echo "No events";
 		endif;
 
-		echo '<div id="posts">';
+		echo '<div id="posts" class="hfeed">';
 		if (have_posts()) :
 			 while (have_posts()) :
-				              echo '<div class="post"';
+				              echo '<article class="post"';
 					the_post();
 					the_content();
-				              echo '</div>';
+				              echo '</article> <!-- .post -->';
 			 endwhile;
 		endif;
-		echo '</div>';
+		echo '</div> <!-- #posts.hfeed -->';
 		?>
-	</div>
-</article>
-<aside role="support_main_content">
+</section> <!--[role=main] -->
+<aside role="complementary">
 <?php
 get_sidebar();
 ?>
