ba541723c413fa3cb1209ccd7948c6f0d4b62281
Nikolai Kristiansen <nikolaik@gmail.com>
2011-11-07 20:05:56 +0100

    Small fix

--- wp-content/themes/neuf-web/single-event.php
+++ wp-content/themes/neuf-web/single-event.php
@@ -14,9 +14,11 @@ if (have_posts()) :
 				<div class="event datetime"><?php echo format_datetime(get_post_meta(get_the_ID(), '_neuf_events_starttime',true)); ?></div>
 				<div class="event price"><?php
 					$price = get_post_meta(get_the_ID(), '_neuf_events_price',true); 
-					echo ($price != "" ? $price : "Gratis");
+					echo ($price ? $price : "Gratis");
 ?>
 				</div>
+				<div class="event venue"><?php echo get_post_meta(get_the_ID(), '_neuf_events_venue',true); ?></div>
+				<div class="event type"><?php echo get_post_meta(get_the_ID(), '_neuf_events_type',true); ?></div>
 			</header> <!-- .event-header -->
 <?php the_content(); ?>
 	        </article> <!-- .event -->
