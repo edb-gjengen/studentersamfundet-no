9f2d876ffba9c7f3c240f6716fbd3753d1d2c7b5
Nikolai Kristiansen <nikolaik@gmail.com>
2011-11-09 02:01:47 +0100

    List subpages in sidebar

--- wp-content/themes/neuf-web/sidebar.php
+++ wp-content/themes/neuf-web/sidebar.php
@@ -1,11 +1,15 @@
 <div id="primary" class="widget-area" role="complementary">
+    <ul>
+        <?php
+        wp_list_pages('title_li=&child_of='.$post->ID); ?>
+    </ul>
         <?php
         /*
          * Content from a custom field available
          * to edit for alle pages.
          */
-        $metas = get_post_meta(get_the_ID(), 'Sidebar Area');
-        echo $metas[0];
+        $metas = get_post_meta($post->ID, 'Sidebar Area');
+        if($metas) echo $metas[0];
         ?>
         <ul class="xoxo">
                 <li><?php wp_loginout(); ?></li>
