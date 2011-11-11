01bbce991bf398bba4c17b771ed03a362568aeca
Nikolai Kristiansen <nikolaik@gmail.com>
2011-11-12 00:06:11 +0100

    Generalize index.php

--- wp-content/themes/neuf-web/index.php
+++ wp-content/themes/neuf-web/index.php
@@ -1,33 +1,35 @@
-<?php
-?>
 <?php get_header(); ?>
 
-        <section id="content" role="main">
+<section id="content" role="main">
 <?php 
-if (have_posts()) :
-    while (have_posts()) : the_post();
-        ?>
-		<article class="event">
-			<header class="event-header">
+if ( have_posts() ) :
+	while (have_posts()) : the_post();
+		?>
+		<article class="<?php echo get_post_type(); ?>">
+			<header>
 				<h1><?php the_title(); ?></h1>
-				<div class="event datetime"><?php echo format_datetime(get_post_meta(get_the_ID(), '_neuf_events_starttime',true)); ?></div>
-				<div class="event price"><?php
+				<?php the_post_thumbnail(); ?>
+				<?php if ( get_post_type() == "event" ): ?>
+				    <div class="datetime"><?php echo format_datetime( get_post_meta( get_the_ID(), '_neuf_events_starttime',  true) ); ?></div>
+				    <div class="price"><?php
 					$price = get_post_meta(get_the_ID(), '_neuf_events_price',true); 
 					echo ($price ? $price : "Gratis");
-?>
-				</div>
-				<div class="event venue"><?php echo get_post_meta(get_the_ID(), '_neuf_events_venue',true); ?></div>
-				<div class="event type"><?php echo get_post_meta(get_the_ID(), '_neuf_events_type',true); ?></div>
+				    ?>
+				    </div>
+				    <div class="venue"><?php echo get_post_meta( get_the_ID(), '_neuf_events_venue', true ); ?></div>
+				    <div class="type"><?php echo get_post_meta( get_the_ID(), '_neuf_events_type', true ); ?></div>
+				<?php endif;?>
 			</header> <!-- .event-header -->
-<?php the_content(); ?>
-	        </article> <!-- .event -->
-<?php
-    endwhile; endif;
+			<?php the_content(); ?>
+		</article> <!-- .event -->
+	<?php
+	endwhile;
+endif;
 ?>
-        </section> <!-- #main_content -->
+</section> <!-- #main_content -->
 
-	<aside id="sidebar">
-<?php get_sidebar(); ?>
-	</aside> <!-- #sidebar -->
+<aside id="sidebar">
+	<?php get_sidebar(); ?>
+</aside> <!-- #sidebar -->
 
 <?php get_footer(); ?>
