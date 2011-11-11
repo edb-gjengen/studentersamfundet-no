ea9372f43233520b07740a9a5b3f8c6b3c1b006c
Robert Kolner <robert.kolner@gmail.com>
2011-11-12 00:57:16 +0100

    Changed program-view layout to more correct one.

--- wp-content/themes/neuf-web/page-program.php
+++ wp-content/themes/neuf-web/page-program.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-		<section id="content" role="main">
+	<section id="content" role="main">
 <?php 
 $events = new WP_Query( array(
 	'post_type' => 'event',
@@ -12,20 +12,58 @@ $events = new WP_Query( array(
 
 if ( $events->have_posts() ) : 
 	$date = "";
+	$last_week = -1;
+	$last_day = -1;
+	$new_day = false;
+	$alt = false;
 	ob_start();
 
 
 	while ( $events->have_posts() ) : $events->the_post();
-		$venue = get_post_meta( $post->ID, '_neuf_events_venue', true);
-		$type  = get_post_meta( $post->ID, '_neuf_events_type', true);
-		$price = get_post_meta( $post->ID, '_neuf_events_price', true);
-		$time = get_post_meta( $post->ID, '_neuf_events_starttime', true);
+		$venue = get_post_meta( $post->ID, '_neuf_events_venue', true );
+		$type  = get_post_meta( $post->ID, '_neuf_events_type', true );
+		$price = get_post_meta( $post->ID, '_neuf_events_price', true );
+		$time = get_post_meta( $post->ID, '_neuf_events_starttime', true );
+		
+		$day = ( int ) strftime("%Y%m%d", $time);
+		$week = ( int ) strftime("%W", $time);
 		?>
-		<article class="<?php get_post_type(); ?>">
-			<header class="page-header">
+
+		<?php
+		if ( $day != $last_day && $new_day ):
+			$new_day = false;
+			if ( $alt )
+				$alt = false;
+			else
+				$alt = true;
+			?>
+			</div>
+			<?php
+		endif;
+		
+		if ( $week != $last_week ):
+			$last_week = $week;
+			?>
+			<h1 class="week-number">Uke <?php echo $week; ?></h1>
+			<?php
+		endif;
+				
+		if ( $day != $last_day ):
+			$last_day = $day;
+			$new_day = true;
+			?>
+			<div class="day <?php if ( $alt ) echo 'alt'; ?>">
+				<h1><?php echo strftime( "%A %e. %B", $time ); ?></h1>
+		<?php
+		endif;
+		?>
+		
+		<article id="<?php the_ID(); ?>">
+			<h1 class="page-header">
+				<a href="<?php echo the_permalink(); ?>"><?php the_post_thumbnail('event-image')?></a>
+				<p><?php echo strftime( "%H:%M", $time ); ?> i <?php echo $venue ?></p>
 				<a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
-			</header> <!-- .page-header -->
-			<?php echo strftime("%Y-%m-%d %H:%M", $time); ?>
+			</h1> <!-- .page-header -->
 		</article> <!-- .page -->
 
 		<?php
