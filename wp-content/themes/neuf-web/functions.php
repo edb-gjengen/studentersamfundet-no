3839f26809e8a6afd1e8725da585e9ce92f6ebde
Just Thomas Misund <t@studentersamfundet.no>
2011-11-12 04:13:40 +0100

    Adding a default (dynamic) sidebar

--- wp-content/themes/neuf-web/functions.php
+++ wp-content/themes/neuf-web/functions.php
@@ -11,6 +11,17 @@ add_theme_support( 'automatic-feed-links' );
 
 register_nav_menu( 'main' , 'Hovedmeny' );
 
+if ( function_exists( 'register_sidebar') ) {
+	register_sidebar( array(
+		'name'          => 'Deafult sidebar',
+		'id'            => 'default-sidebar',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<header><h1>',
+		'after_title'   => '</h1></header>'
+	) );
+}
+
 /* Format a unix timestamp respecting the options set in Settings->General. */
 if(!function_exists('format_datetime')) {
 	function format_datetime( $timestamp ) {
--- wp-content/themes/neuf-web/sidebar.php
+++ wp-content/themes/neuf-web/sidebar.php
@@ -1,7 +1,12 @@
 <aside id="primary" class="widget-area" role="complementary">
+
+	<?php
+	$children = wp_list_pages( 'title_li=&child_of=' . $post->ID . '&echo=0' );
+	if ($children) { ?>
 	<ul>
-		<?php wp_list_pages( 'title_li=&child_of=' . $post->ID ); ?>
+	<?php echo $children; ?>
 	</ul>
+	<?php } // if $children ?>
 
 	<?php
 	/**
@@ -12,8 +17,12 @@
 	if($metas) echo $metas[0];
 	?>
 
+	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'default-sidebar' ) ) : // if no 'default-sidebar' ?>
+
 	<ul class="xoxo">
 		<li><?php wp_loginout(); ?></li>
 	</ul>
+
+	<?php endif; // no default-sidebar ?>
 
 </aside><!-- #primary .widget-area -->
