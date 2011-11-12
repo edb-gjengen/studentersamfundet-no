19bf28038676c4f1013bf1922bbd86f89576e7df
Just Thomas Misund <t@studentersamfundet.no>
2011-11-12 07:17:24 +0100

    Moving images to img

--- wp-content/themes/neuf-web/footer.php
+++ wp-content/themes/neuf-web/footer.php
@@ -2,32 +2,32 @@
 	<ul id="supporters">
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/uio.png" alt="Universitetet i Oslo">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/uio.png" alt="Universitetet i Oslo">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/studio.png" alt="Studentfestivalen i Oslo">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/studio.png" alt="Studentfestivalen i Oslo">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/logo_sh86.png" alt="Studenthovedstaden">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/logo_sh86.png" alt="Studenthovedstaden">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/toro.png" alt="Toro">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/toro.png" alt="Toro">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/akademika.png" alt="Akademika">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/akademika.png" alt="Akademika">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="<?= bloginfo('stylesheet_directory'); ?>/images/logo/nrf.png" alt="Norsk Rockforbund">
+				<img src="<?= bloginfo('stylesheet_directory'); ?>/img/logo/nrf.png" alt="Norsk Rockforbund">
 			</a>
 		</li>
 	</ul>
