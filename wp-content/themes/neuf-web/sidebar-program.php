7f02ca82c0c8a08867b3158588ac8bd8f47abe6f
Robert Kolner <robert.kolner@gmail.com>
2011-11-12 04:38:51 +0100

    Updated program-sidebar.

--- wp-content/themes/neuf-web/sidebar-program.php
+++ wp-content/themes/neuf-web/sidebar-program.php
@@ -7,30 +7,6 @@
 
 <aside id="primary" class="widget-area" role="complementary">
 	<?php
-	$types = array(
-		'Annet' ,'Debatt','Fest','Film','Foredag',
-		'Forfatteraften','Klubb','Konsert','Quiz',
-		'Teater','Upop','Stand-up'
-	);
-	?>
-	<ul id="type-filter" class="sidebar-item">
-		<?php
-			foreach ( $types as $type ):
-				?>
-				<li>
-					<a onclick="events_toggle('<?php echo $type ?>')"><?php echo $type; ?></a>
-				</li>
-				<?php
-			endforeach;
-		?>
-	</ul>
-
-
-	<ul>
-		<?php wp_list_pages( 'title_li=&child_of=' . $post->ID ); ?>
-	</ul>
-
-	<?php
 	/**
 	 * Content from a custom field available
 	 * to edit for all pages.
@@ -38,6 +14,33 @@
 	$metas = get_post_meta($post->ID, 'Sidebar Area');
 	if($metas) echo $metas[0];
 	?>
+
+	<?php
+	$types = array(
+		'Annet' ,'Debatt','Fest','Film','Foredag',
+		'Forfatteraften','Klubb','Konsert','Quiz',
+		'Teater','Upop','Stand-up'
+	);
+	?>
+	
+	<?php if ( !function_exists( 'program_sidebar' ) || !dynamic_sidebar( 'program-sidebar' ) ) : // if no 'program-sidebar' ?>
+		<ul id="type-filter" class="widget">
+			<?php
+				foreach ( $types as $type ):
+					?>
+					<li>
+						<a onclick="events_toggle('<?php echo $type ?>')"><?php echo $type; ?></a>
+					</li>
+					<?php
+				endforeach;
+			?>
+		</ul>
+	<?php endif; ?>
+
+
+	<ul>
+		<?php wp_list_pages( 'title_li=&child_of=' . $post->ID ); ?>
+	</ul>
 
 	<ul class="xoxo">
 		<li><?php wp_loginout(); ?></li>
