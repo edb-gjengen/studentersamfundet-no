8192c451bb75bdf8a3c2022dfd735c2096311d40
Nikolai Kristiansen <nikolaik@gmail.com>
2011-11-12 05:46:11 +0100

    Adds a slider with featured events on frontpage.

--- wp-content/themes/neuf-web/js/program.js
+++ wp-content/themes/neuf-web/js/program.js
@@ -45,3 +45,12 @@ function events_toggle( type ) {
 		}
 	});
 }
+$(function() {
+		$("#slider").cycle({
+			fx:     'fade', 
+			speed:  'fast', 
+			timeout: 4000, 
+			next:   '#snext', 
+			prev:   '#sprev' 
+			});
+		});
