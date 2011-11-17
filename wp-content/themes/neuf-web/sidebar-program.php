4b0d3d43b48c7d70c0037fac4dbffb6a37e2c5ec
Just Thomas Misund <t@studentersamfundet.no>
2011-11-17 16:09:15 +0100

    Making sidebar-program show up again

--- wp-content/themes/neuf-web/sidebar-program.php
+++ wp-content/themes/neuf-web/sidebar-program.php
@@ -12,37 +12,34 @@
 	$types = get_terms( 'event_type', array(
 		'hide_empty' => 0
 	) );
+	$count = count($types);
+	if ($count > 0):
 	?>
 	
-	<?php 
-	if ( !function_exists( 'program_sidebar' ) || !dynamic_sidebar( 'program-sidebar' ) ): 
-		$count = count($types);
-		if ($count > 0):
-	?>
-	
-			<ul id="type-filter" class="widget">
-				<?php
-					foreach ( $types as $type ):
-						?>
-						<li>
-							<a href='#' onclick="events_toggle('<?php echo $type->name ?>')"><?php echo $type->name; ?></a>
-						</li>
-						<?php
-					endforeach;
-				?>
-			</ul>
-			<?php 
-		endif;
-	endif; 
-	?>
+	<ul id="type-filter" class="widget">
+		<?php foreach ( $types as $type ): ?>
 
+		<li>
+			<a href='#' onclick="events_toggle('<?php echo $type->name ?>')"><?php echo $type->name; ?></a>
+		</li>
+		<?php endforeach; ?>
 
-	<ul>
+	</ul>
+
+	<?php endif; // $count ?>
+
+	<ul class="xoxo">
 		<?php wp_list_pages( 'title_li=&child_of=' . $post->ID ); ?>
 	</ul>
+
+	<?php // Dynamic sidebar starts here ?>
+	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'program-sidebar' ) ) : // if no 'program-sidebar' ?>
 
 	<ul class="xoxo">
 		<li><?php wp_loginout(); ?></li>
 	</ul>
+
+	<?php endif; // no program-sidebar ?>
+
 
 </aside><!-- #primary .widget-area -->
