c17dde1614d280e1ae9616ae9bbf3004e10acb37
Martin Folkeseth <martin.folkseth@gmail.com>
2011-11-29 18:09:30 +0100

    Animasjonene på events er satt til raskere

--- wp-content/themes/neuf-web/js/front-page.js
+++ wp-content/themes/neuf-web/js/front-page.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	var events = $('#events');
 	var articles = events.find('article').each(function(){
 		$(this).hover(function(){
-			$(this).find('header.info').children().slideDown();
+			$(this).find('header.info').children().slideDown('fast');
 		},
 		function(){
-			$(this).find('header.info').find('.price').slideUp();
-			$(this).find('header.info').find('.venue').slideUp();
-			$(this).find('header.info').find('.type').slideUp();
+			$(this).find('header.info').find('.price').slideUp('fast');
+			$(this).find('header.info').find('.venue').slideUp('fast');
+			$(this).find('header.info').find('.type').slideUp('fast');
 		});
 	});
 });
