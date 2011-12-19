63b30441f5647e8fe2b0ba9647a6c522d917d054
Just Thomas Misund <t@studentersamfundet.no>
2011-12-19 18:49:22 +0100

    Fitting opening hours into slimmer header. Also changing some perms.

--- wp-content/themes/neuf-old-style/header.php
+++ wp-content/themes/neuf-old-style/header.php
@@ -31,29 +31,33 @@
             <?php display_quick_menu(); // defined in functions.php ?>
 
             <div class="aapningstider">
-                <table>
-                    <tr>
-						<td>Mandag - tirsdag</td>
-						<td style="text-align:right;">13.00 - 01.00</td>
-					</tr>
-					<tr>
-						<td>Onsdag</td>
-						<td style="text-align:right;">- 01.30</td>
-					</tr>
-					<tr>
-						<td>Torsdag - fredag</td>
-						<td style="text-align:right;">- 03.00</td>
-					</tr>
-					<tr>
-						<td>Lørdag</td>
-						<td style="text-align:right;">15.00 - 03.00</td>
-					</tr>
-                   			<tr>
-						<td>Kjøkkenet</td>
-						<td style="text-align:right;">- 19.00</td>
-					</tr>
-		<tr id="bokkafeen_tider"><td><a href="http://studentersamfundet.no/foreninger.php?id=3">BokCaféen</a> <span style="color:#888888; font-style:italic;">i dag</span></td>
-			<td style="text-align:right;"><?php
+		<table>
+			<tr>
+				<th colspan="2" style="text-align:right;">Åpningstider</th>
+			</tr>
+			<tr>
+				<td>Mandag - tirsdag</td>
+				<td style="text-align:right;">13.00 - 01.00</td>
+			</tr>
+			<tr>
+				<td>Onsdag</td>
+				<td style="text-align:right;">- 01.30</td>
+			</tr>
+			<tr>
+				<td>Torsdag - fredag</td>
+				<td style="text-align:right;">- 03.00</td>
+			</tr>
+			<tr>
+				<td>Lørdag</td>
+				<td style="text-align:right;">15.00 - 03.00</td>
+			</tr>
+			<tr>
+				<td>Kjøkkenet</td>
+				<td style="text-align:right;">- 19.00</td>
+			</tr>
+			<tr id="bokkafeen_tider">
+				<td><a href="http://studentersamfundet.no/foreninger.php?id=3">BokCaféen</a> <span style="color:#888888; font-style:italic;">i dag</span></td>
+				<td style="text-align:right;"><?php
 
 $tider = array('Stengt',
 		'19.00 - 00.00',
@@ -67,7 +71,9 @@ $day = date('w');
 echo '<a href="http://studentersamfundet.no/foreninger.php?id=3">';
 echo $tider[$day];
 echo '</a>';
-?></td></tr>
+?>
+				</td>
+			</tr>
                 </table>
             </div>
 
