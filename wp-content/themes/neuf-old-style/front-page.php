b45ae004e7ee5fee5205209930065db8f607bc30
Just Thomas Misund <t@studentersamfundet.no>
2012-01-30 21:31:40 +0100

    Fixes semantics in article section on front page.

--- wp-content/themes/neuf-old-style/front-page.php
+++ wp-content/themes/neuf-old-style/front-page.php
@@ -15,19 +15,15 @@ get_header();
 
 <?php get_template_part( 'program' , '3days' ); ?>
 
-<?php get_template_part( 'program' , '6days' ); ?>
+<?php // get_template_part( 'program' , '6days' ); ?>
 
 		<div class="clearfix"></div>
 
-		<div class="grid_8">
-			<h3><a href="<?php bloginfo('url'); ?>/aktuelt/">Aktuelt</a></h3>
-		</div>
-		<div class="grid_4">
-			<h3><a href="<?php bloginfo('url'); ?>/program/">Program</a></h3>
-		</div>
-		<div id="articles" class="hfeed">
+		<div id="articles" class="grid_8 hfeed">
 
-			<div class="grid_4">
+			<h3><a href="<?php bloginfo('url'); ?>/aktuelt/">Aktuelt</a></h3>
+
+			<div class="grid_4 alpha">
 				<?php // The LOOP
 					query_posts('posts_per_page=6');
 					$counter = 1;
@@ -47,7 +43,7 @@ get_header();
 						if($counter == 3) {
 					?>
 					</div>
-					<div class="grid_4">
+					<div class="grid_4 omega">
 					<?php
 					}
 					$counter++;
@@ -59,36 +55,33 @@ get_header();
 
 		</div> <!-- #articles -->
 
-		<div id="events">
-			<div class="grid_4">
-				<?php 
-					$meta_query = array(
-						'key'     => '_neuf_events_starttime',
-						'value'   => date( 'U' , strtotime( '-8 hours' ) ), 
-						'compare' => '>',
-						'type'    => 'numeric'
-					);
+		<div id="events" class="grid_4">
+			<h3><a href="<?php bloginfo('url'); ?>/program/">Program</a></h3>
+			<?php 
+				$meta_query = array(
+					'key'     => '_neuf_events_starttime',
+					'value'   => date( 'U' , strtotime( '-8 hours' ) ), 
+					'compare' => '>',
+					'type'    => 'numeric'
+				);
 
-					$args = array(
-						'post_type'      => 'event',
-						'meta_query'     => array( $meta_query ),
-						'posts_per_page' => 10,
-						'orderby'        => 'meta_value_num',
-						'meta_key'       => '_neuf_events_starttime',
-						'order'          => 'ASC'
-					);
+				$args = array(
+					'post_type'      => 'event',
+					'meta_query'     => array( $meta_query ),
+					'posts_per_page' => 10,
+					'orderby'        => 'meta_value_num',
+					'meta_key'       => '_neuf_events_starttime',
+					'order'          => 'ASC'
+				);
 
-					$events = new WP_Query( $args );
-					if ( $events->have_posts() ) : while ( $events->have_posts() ) : $events->the_post();
-				?>
+				$events = new WP_Query( $args );
+				if ( $events->have_posts() ) : while ( $events->have_posts() ) : $events->the_post();
+			?>
 
-					<div id="post-<?php the_ID(); ?>" <?php neuf_post_class(); ?>>
-						<?php $date = get_post_meta(get_the_ID(), '_neuf_events_starttime'); echo neuf_event_format_date($date[0]); ?> <a href="<?php the_permalink(); ?>" title="Permalenke til <?php the_title(); ?>"><?php the_title(); ?></a>
-					</div>
-					<?php endwhile; ?>
+				<div id="post-<?php the_ID(); ?>" <?php neuf_post_class(); ?>>
+					<?php $date = get_post_meta(get_the_ID(), '_neuf_events_starttime'); echo neuf_event_format_date($date[0]); ?> <a href="<?php the_permalink(); ?>" title="Permalenke til <?php the_title(); ?>"><?php the_title(); ?></a>
 				</div>
-			<?php endif; ?>
-			</div> <!-- .third_column -->
+				<?php endwhile; endif; ?>
 
 		</div> <!-- #events -->
 
