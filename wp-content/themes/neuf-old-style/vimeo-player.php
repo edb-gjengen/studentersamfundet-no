9dd28facbd9882c46f583bdf89d9b48c213f6904
Robert Kolner <robert.kolner@gmail.com>
2012-02-12 23:37:57 +0100

    Added vimeo-player template that plays the latest video from ostv.

---
+++ wp-content/themes/neuf-old-style/vimeo-player.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template name: Vimeo player
+*/
+$url = 'http://vimeo.com/api/v2/ostv/videos.php';
+$v = unserialize(file_get_contents($url));
+$video_id = $v[0]['id'];
+?>
+<iframe 
+	src="http://player.vimeo.com/video/<?php echo $video_id;?>?title=0&amp;byline=0&amp;portrait=0" 
+	width="400" 
+	height="225" 
+	frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen>
+</iframe>
