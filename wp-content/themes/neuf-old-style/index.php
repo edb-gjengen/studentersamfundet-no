c0816217de3c730d497b253ac250d685aee609a5
Just Thomas Misund <t@studentersamfundet.no>
2012-03-06 22:29:32 +0100

    Adding semantic post classes. Removing old and unused css.

--- wp-content/themes/neuf-old-style/index.php
+++ wp-content/themes/neuf-old-style/index.php
@@ -12,7 +12,7 @@
 
 		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-			<div class="hentry">
+			<div <?php post_class(); ?>>
 
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 				<div class="entry-meta byline"><span class="meta-prep meta-prep-author">Av </span><span class="author vcard"><?php the_author_link(); ?></span><span class="meta-sep meta-sep-entry-date"> | </span><span class="meta-prep meta-prep-entry-date">Publisert: </span><span class="entry-date"><?php the_time('Y-m-d G:i'); ?></span></div>
