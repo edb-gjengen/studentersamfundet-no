93d0d28675c79bb4a08bb2514caef2e126f2e32f
Nikolai Kristiansen <nikolaik@gmail.com>
2012-03-20 03:57:20 +0100

    Page style fixes

--- wp-content/themes/neuf-old-style/index.php
+++ wp-content/themes/neuf-old-style/index.php
@@ -3,34 +3,38 @@
 		<div id="content" class="container_12">
 
 		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-
+			
 			<article <?php neuf_post_class(); ?>>
 
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<div class="entry-meta byline"><span class="meta-prep meta-prep-author">Av </span><span class="author vcard"><?php the_author_link(); ?></span><span class="meta-sep meta-sep-entry-date"> | </span><span class="meta-prep meta-prep-entry-date">Publisert: </span><span class="entry-date"><?php the_time('Y-m-d G:i'); ?></span></div>
+				<div class="grid_10">
 
-<?php
-	$attachments = get_posts( array( 'post_type' => 'attachment' , 'numberposts' => -1 , 'post_status' => null , 'post_parent' => $post->ID ) );
-	if ( $attachments ) {
-?>
-				<div class="vedlegg">
-<?php
-		foreach ( $attachments as $attachment ) {
-?>
-					<div class="nyhetsbilde">
-			<?php the_attachment_link( $attachment->ID ); ?>
-						<?php /*if ($attachment['caption']) { ?>
-						<div class="caption"><?php echo($attachment['caption']); ?></div>
-			<?php } */ ?>
-					</div> <!-- .nyhetsbilde -->
-					<?php }  // end foreach attachment ?>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<div class="entry-meta byline"><span class="meta-prep meta-prep-author">Av </span><span class="author vcard"><?php the_author_link(); ?></span><span class="meta-sep meta-sep-entry-date"> | </span><span class="meta-prep meta-prep-entry-date">Publisert: </span><span class="entry-date"><?php the_time('Y-m-d G:i'); ?></span></div>
 
-				</div> <!-- .vedlegg -->
-				<?php } // end if attachments ?>
+	<?php
+		$attachments = get_posts( array( 'post_type' => 'attachment' , 'numberposts' => -1 , 'post_status' => null , 'post_parent' => $post->ID ) );
+		if ( $attachments ) {
+	?>
+					<div class="vedlegg">
+	<?php
+			foreach ( $attachments as $attachment ) {
+	?>
+						<div class="nyhetsbilde">
+				<?php the_attachment_link( $attachment->ID ); ?>
+							<?php /*if ($attachment['caption']) { ?>
+							<div class="caption"><?php echo($attachment['caption']); ?></div>
+				<?php } */ ?>
+						</div> <!-- .nyhetsbilde -->
+						<?php }  // end foreach attachment ?>
 
-				<div class="entry-content"><?php the_content(); ?></div> <!-- .entry-content -->
+					</div> <!-- .vedlegg -->
+					<?php } // end if attachments ?>
 
-				<?php display_social_sharing_buttons(); ?>
+					<div class="entry-content"><?php the_content(); ?></div> <!-- .entry-content -->
+
+					<?php display_social_sharing_buttons(); ?>
+
+				</div>
 
 			</article> <!-- .post -->
 
