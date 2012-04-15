68b173d011dff1a5214c6ec58915c47ba713ff31
Nikolai Kristiansen <nikolaik@gmail.com>
2012-04-15 23:59:39 +0200

    Foreningene: Style fixes

--- wp/wp-content/themes/neuf-old-style/page-foreningene.php
+++ wp/wp-content/themes/neuf-old-style/page-foreningene.php
@@ -15,13 +15,13 @@ $args = array(
 $associations = new WP_Query( $args );
 
 if ( $associations->have_posts() ) : ?>
-	<div class="grid_12">
+	<div id="associations" class="grid_12">
 	<div class="association-row">
 	<?php while ( $associations->have_posts() ) : $associations->the_post();
 		if ( $associations->current_post%4==0 && $associations->current_post != 0)
 			echo '</div><div class="association-row">'; ?>
 		<div class="association">
-			<a href="<?php the_permalink(); ?>"><?php echo has_post_thumbnail() ? get_the_post_thumbnail( $ID, 'association-thumb' ) : "<h2>".$post->post_title."</h2>"; ?></a>
+			<a href="<?php the_permalink(); ?>"><?php echo has_post_thumbnail() ? get_the_post_thumbnail( $post->ID, 'association-thumb' ) : "<h2>".$post->post_title."</h2>"; ?></a>
 		</div>
 	<?php endwhile; ?>
 	</div>
