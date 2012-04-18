e7f86a59b33513b1523e847f02636ab8585955e3
Paal Braathen <paalbra@gmail.com>
2012-04-18 23:06:21 +0200

    Removed random footer row randomness

--- wp/wp-content/themes/neuf-old-style/footer.php
+++ wp/wp-content/themes/neuf-old-style/footer.php
@@ -1,8 +1,9 @@
 
 <footer id="site-footer">
-        <div id="footer-row1" class="container_12">
+	<div class="container_12">
+        <div class="grid_12">
 
-            <div id="footer-about" class="grid_4">
+            <div id="footer-about" class="grid_4 alpha">
                 <h2>Det Norske Studentersamfund</h2>
                 <p>
                     Studentene i Oslo har sitt naturlige tilholdssted p&aring; Det Norske Studentersamfund, i hyggelige lokaler p&aring; Chateau Neuf &oslash;verst p&aring; Majorstuen. Her er det &aring;pent alle dager unntatt s&oslash;ndag, og enten man &oslash;nsker en tur i baren, p&aring; kaf&eacute;, p&aring; debatt, p&aring; konsert, teater eller kino, har man muligheten p&aring; Det Norske Studentersamfund.
@@ -14,18 +15,17 @@
             <div id="fb-like-box" class="grid_4">
                 <div class="fb-like-box" data-href="https://www.facebook.com/studentersamfundet" data-width="315" data-height="285" data-show-faces="true" data-stream="false" data-header="false"></div>
             </div>
-            <div class="grid_4">
-                <h2><a href="https://twitter.com/dns1813">Fra Twitter</a></h2>
+            <div id="twitter-box" class="grid_4 omega">
+                <!--h2><a href="https://twitter.com/dns1813">Fra Twitter</a></h2>
                 <div id="twitter_feed"></div> <!-- #twitter_feed -->
-                <h2><a href='http://www.flickr.com/groups/neuf/pool/'>Bilder fra flickr</a></h2>
+                <h2><a href='http://www.flickr.com/groups/neuf/pool/'>Bilder fra flickr</a></h2--!>
                 <div id="flickr-images">
                     <ul id="flickr_feed"></ul>
                 </div>
             </div> 
-
         </div>
 
-        <div id="footer-row2" class="container_12">
+        <div class="grid_12">
             <div id="sponsors">
 
                 <div id="sponsor-uio" class="sponsor">
@@ -47,16 +47,16 @@
             </div>
         </div>
 
-	<div id="kolofon" class="container_12">
+	<div id="kolofon" class="grid_12">
 
-            <span class="credits grid_12">Det Norske Studentersamfund | Slemdalsveien 15, 0369 Oslo | Webdesign av <a href="http://edb.neuf.no">EDB-gjengen</a> og <a href="#">Designerne</a> i KAK</span>
+            <span class="">Det Norske Studentersamfund | Slemdalsveien 15, 0369 Oslo | Webdesign av <a href="http://edb.neuf.no">EDB-gjengen</a> og <a href="#">Designerne</a> i KAK</span>
 
 	</div> <!-- #kolofon -->
 
 	<!-- Google Analytics -->
 	<script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."); document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script type="text/javascript">try {var pageTracker = _gat._getTracker("UA-52914-1");pageTracker._trackPageview();} catch(err) {}</script>
 	<!-- end Google Analytics -->
-
+	</div>
 </footer> <!-- #site-footer -->
 
 <?php wp_footer(); ?>
