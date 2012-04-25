961cb0425bf8ab5c1b13f5b4045f9eff2b51e40a
Nikolai Kristiansen <nikolaik@gmail.com>
2012-04-25 22:27:32 +0200

    Opening hours in own page.

--- wp/wp-content/themes/neuf-old-style/menu.php
+++ wp/wp-content/themes/neuf-old-style/menu.php
@@ -1,48 +1,21 @@
 				<nav id="menu" class="grid_6">
 					<ul class="aapningstider">
-						<li><a href="#">Åpningstider &#9662;</a>
+						<li><a href="/aapningstider/">Åpningstider &#9662;</a>
 							<ul>
 								<li>
 									<table>
 										<tbody>
 											<tr>
-												<td>Man - tirs</td>
-												<td class="right">13.00 - 01.00</td>
-											</tr>
-											<tr>
-												<td>Onsdag</td>
-												<td class="right">13.00 - 01.30</td>
+												<td>Man - ons</td>
+												<td class="right">11.00 - 01.00</td>
 											</tr>
 											<tr>
 												<td>Tors - fre</td>
-												<td class="right">13.00 - 03.00</td>
+												<td class="right">11.00 - 03.00</td>
 											</tr>
 											<tr>
 												<td>Lørdag</td>
-												<td class="right">15.00 - 03.00</td>
-											</tr>
-											<tr>
-												<td>Kjøkkenet</td>
-												<td class="right">- 19.00</td>
-											</tr>
-											<tr id="bokkafeen_tider">
-												<td><a href="http://studentersamfundet.no/foreninger.php?id=3">BokCaféen</a></td>
-												<td class="right"><?php
-
-													$tider = array('Stengt',
-															'19.00 - 00.00',
-															'19.00 - 00.00',
-															'19.00 - 00.00',
-															'19.00 - 03.00',
-															'19.00 - 03.00',
-															'20.00 - 03.00');
-
-													$day = date('w');
-													echo '<a href="http://studentersamfundet.no/foreninger.php?id=3">';
-													echo $tider[$day];
-													echo '</a>';
-													?>
-												</td>
+												<td class="right">16.00 - 03.00</td>
 											</tr>
 										</tbody>
 									</table>
