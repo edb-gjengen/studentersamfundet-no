4d361d5364676bb6ce09f3b574c901303b6380bc
Just Thomas Misund <t@studentersamfundet.no>
2012-05-03 00:44:40 +0200

    Fixing up index.php. Indenting correctly. Cleaning up a bit.

--- wp/wp-content/themes/neuf-old-style/index.php
+++ wp/wp-content/themes/neuf-old-style/index.php
@@ -2,36 +2,27 @@
 
 		<div id="content" class="container_12">
 
+			<h1 class="page-title grid_12"><?php the_title(); ?></h1>
+
 		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 			
 			<article <?php neuf_post_class(); ?>>
 
-				<div class="grid_10">
+				<div class="grid_6">
 
-					<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+					<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
-	<?php
-		$attachments = get_posts( array( 'post_type' => 'attachment' , 'numberposts' => -1 , 'post_status' => null , 'post_parent' => $post->ID ) );
-		if ( $attachments ) {
-	?>
+					<div class="entry-content"><?php the_excerpt(); ?></div> <!-- .entry-content -->
+
+				</div> <!-- .grid_6 -->
+
+				<div class="grid_6">
+
 					<div class="vedlegg">
-	<?php
-			foreach ( $attachments as $attachment ) {
-	?>
-						<div class="nyhetsbilde">
-				<?php the_attachment_link( $attachment->ID ); ?>
-							<?php /*if ($attachment['caption']) { ?>
-							<div class="caption"><?php echo($attachment['caption']); ?></div>
-				<?php } */ ?>
-						</div> <!-- .nyhetsbilde -->
-						<?php }  // end foreach attachment ?>
 
-					</div> <!-- .vedlegg -->
-					<?php } // end if attachments ?>
+					<?php the_post_thumbnail( 'six-column-slim' , array( 'style' => 'display:block;margin:auto;' ) ); ?>
 
-					<div class="entry-content"><?php the_content(); ?></div> <!-- .entry-content -->
-
-				</div>
+				</div> <!-- .grid_6 -->
 
 			</article> <!-- .post -->
 
