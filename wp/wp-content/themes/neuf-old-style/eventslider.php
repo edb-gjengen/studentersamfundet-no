2267f68f64daab7186c0440b73db6f1c1b8a67d8
Robert Kolner <robert.kolner@gmail.com>
2012-05-08 19:59:19 +0200

    Removed news from eventslider.

--- wp/wp-content/themes/neuf-old-style/eventslider.php
+++ wp/wp-content/themes/neuf-old-style/eventslider.php
@@ -26,8 +26,6 @@ $args = array(
 );
 
 $events = new WP_Query( $args );
-
-$news = new WP_Query( 'type=post' );
 ?>
 <?php if ($events->have_posts()) : ?>
 	<section id="featured" class="clearfix">
@@ -35,21 +33,7 @@ $news = new WP_Query( 'type=post' );
 		<a href="#" id="nextLink">Neste</a>
 		<div id="slidernav"></div>
 		<div id="slider"> 
-		<?php
-		if ($news->have_posts()) : $news->the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php neuf_post_class(); ?>>
-				<a class="permalink blocklink" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
-					<header class="grid_6">
-						<h1><?php the_title(); ?></h1>
-						<?php the_excerpt(); ?>
-					</header>
-					<div class="grid_6">
-						<?php the_post_thumbnail( 'six-column-promo' ); ?>
-					</div>
-				</a>
-			</article> <!-- #post-<?php the_ID(); ?> -->
 
-		<?php endif; // $news->have_posts() ?>
 		<?php $counter = 0;
 		while ($events->have_posts() && $counter < 4) : $events->the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php neuf_post_class(); ?>>
