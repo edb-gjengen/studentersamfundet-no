95420bdd1251b1360b528470d8cae2126773ec84
Nikolai Kristiansen <nikolaik@gmail.com>
2012-05-19 16:36:20 +0200

    Adds datetime to articles

--- wp/wp-content/themes/neuf-old-style/digest.php
+++ wp/wp-content/themes/neuf-old-style/digest.php
@@ -10,8 +10,9 @@
 				?>
 
 					<article id="post-<?php the_ID(); ?>" <?php neuf_post_class(); ?>>
-				<a class="permalink" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                                                <div class="entry-summary"><?php echo linkify(trim_excerpt(get_the_excerpt(), 30), '/\[\.\.\.\]/', get_permalink()); ?></div>
+                        <a class="permalink" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                        <div class="when">Publisert <?php the_date()?> <?php the_time(); ?></div>
+                        <div class="entry-summary"><?php echo linkify(trim_excerpt(get_the_excerpt(), 30), '/\[\.\.\.\]/', get_permalink()); ?></div>
 					</article>
 					<?php
 						if($digest_news_counter == 3) {
