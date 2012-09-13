befcf2c45c86f3f79131378179654299362e2800
Just Thomas Misund <t@studentersamfundet.no>
2012-09-13 21:04:24 +0200

    Adding endless scrolling on event type template.

--- wp/wp-content/themes/neuf-old-style/taxonomy-event_type.php
+++ wp/wp-content/themes/neuf-old-style/taxonomy-event_type.php
@@ -27,7 +27,7 @@ $args = array(
 	'post_type'      => 'event',
 	'meta_query'     => array( $meta_query ),
 	'tax_query'      => array( $tax_query ),
-	'posts_per_page' => 10,
+	'posts_per_page' => get_option('posts_per_page'),
 	'orderby'        => 'meta_value_num',
 	'meta_key'       => '_neuf_events_starttime',
 	'order'          => 'ASC'
@@ -52,34 +52,102 @@ $past = new WP_Query( $args )
 
 			<section id="future-events" class="grid_6">
 				<header>
-					<h2>Framtidige arrangementer</h2>
+					<h2><?php echo $term->name ?> i framtida</h2>
 				</header>
 
-				<?php if( $future->have_posts() ) : while( $future->have_posts() ) : $future->the_post(); ?>
-					
-					<?php get_template_part( 'loop', 'taxonomy-event_type' ); ?>
-
-				<?php endwhile; else: ?>
-					<p>Ingen flere arrangementer.</p>
-				<?php endif; ?>
+				<?php $wp_query = $future; ?>
+				<?php get_template_part( 'loop', 'taxonomy-event_type' ); ?>
 
 			</section> <!-- #future-events -->
 
 			<section id="past-events" class="grid_6">
 				<header>
-					<h2>Tidligere arrangementer</h2>
+					<h2><?php echo $term->name ?> i fortida</h2>
 				</header>
 
-				<?php if( $past->have_posts() ) : while( $past->have_posts() ) : $past->the_post(); ?>
-					
-					<?php get_template_part( 'loop', 'taxonomy-event_type' ); ?>
-
-				<?php endwhile; else: ?>
-					<p>Ingen flere arrangementer.</p>
-				<?php endif; ?>
+				<?php $wp_query = $past; ?>
+				<?php get_template_part( 'loop', 'taxonomy-event_type' ); ?>
 
 			</section> <!-- #future-events -->
 
 </div> <!-- #content -->
+
+<script type="text/javascript">
+
+	var nextPastPage   = 2;
+	var nextFuturePage = 2;
+	var pastLoading   = false;
+	var futureLoading = false;
+	var pastTotal   = <?php echo   $past->max_num_pages; ?>;
+	var futureTotal = <?php echo $future->max_num_pages; ?>;
+	var pastLoader   =   "<img id='past-loader' alt='' src='<?php bloginfo('template_directory'); ?>/img/ajax-loader.gif' align='center' />";
+	var futureLoader = "<img id='future-loader' alt='' src='<?php bloginfo('template_directory'); ?>/img/ajax-loader.gif' align='center' />";
+
+	$(window).scroll(function() {
+		// If we reach the bottom of the #past-events section
+		if ( $(window).scrollTop() + $(window).height() > $('#site-header').height() + $('#past-events').height() - $('#site-footer').height() ) {
+			// And we're not already loading or have reached the end
+			if ( pastLoading || nextPastPage > pastTotal ) {
+				return false;
+			} else {
+				// load more posts
+				pastLoading = true;
+				loadMorePastEvents(nextPastPage);
+				nextPastPage++;
+				console.log('loading more past events...');
+			}
+		}
+
+		// If we reach the bottom of the #future-events section
+		if ( $(window).scrollTop() + $(window).height() > $('#site-header').height() + $('#future-events').height() - $('#site-footer').height() ) {
+			// And we're not already loading or have reached the end
+			if ( futureLoading || nextFuturePage > futureTotal ) {
+				return false;
+			} else {
+				// load more posts
+				futureLoading = true;
+				loadMoreFutureEvents(nextFuturePage);
+				nextFuturePage++;
+				console.log('loading more future events...');
+			}
+		}
+	});
+
+	function loadMorePastEvents(pageNumber) {
+		if ( 0 == $('#past-loader').length ) {
+			$('#past-events').append(pastLoader);
+		}
+		$.ajax({
+			url:"<?php bloginfo('wpurl'); ?>/wp-admin/admin-ajax.php",
+			type: 'POST',
+			data: "action=infinite_scroll&page=" + pageNumber + '&time_scope=past&term=<?php echo get_query_var('term'); ?>&template=loop-taxonomy-event_type',
+			success: function(html){
+				$('#past-loader').hide(2000);
+				$('#past-loader').remove();
+				$('#past-events').append(html);
+				pastLoading = false;
+			}
+		});
+		return false;
+	}
+
+	function loadMoreFutureEvents(pageNumber) {
+		if ( 0 == $('#future-loader').length ) {
+			$('#future-events').append(futureLoader);
+		}
+		$.ajax({
+			url:"<?php bloginfo('wpurl'); ?>/wp-admin/admin-ajax.php",
+			type: 'POST',
+			data: "action=infinite_scroll&page=" + pageNumber + '&time_scope=future&term=<?php echo get_query_var('term'); ?>&template=loop-taxonomy-event_type',
+			success: function(html){
+				$('#future-loader').hide(2000);
+				$('#future-loader').remove();
+				$('#future-events').append(html);
+				futureLoading = false;
+			}
+		});
+		return false;
+	}
+</script>
 
 <?php get_footer(); ?>
