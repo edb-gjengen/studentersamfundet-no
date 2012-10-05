971c0229990c57b2caa2b52a6c4d966afad4adf7
Nikolai Kristiansen <nikolaik@gmail.com>
2012-10-05 15:19:36 +0200

    Forgot wordpress template thing

--- wp/wp-content/themes/neuf-old-style/page-ninjanewsletter.php
+++ wp/wp-content/themes/neuf-old-style/page-ninjanewsletter.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Template Name: Ninjanewsletter
+ */
+
 $utfpage = file_get_contents("http://studentersamfundet.no/nyhetsbrev");
 $utfpage = str_replace("charset=UTF-8","charset=ISO-8859-1", $utfpage);
 $isopage = utf8_decode($utfpage);
