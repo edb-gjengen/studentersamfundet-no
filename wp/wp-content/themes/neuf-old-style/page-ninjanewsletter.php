4c16883357552bae6fe724921f689305d63755be
Nikolai Kristiansen <nikolaik@gmail.com>
2012-10-05 15:15:48 +0200

    Adds temp ninjafix for maillit.no-newsletters

---
+++ wp/wp-content/themes/neuf-old-style/page-ninjanewsletter.php
@@ -0,0 +1,7 @@
+<?php
+$utfpage = file_get_contents("http://studentersamfundet.no/nyhetsbrev");
+$utfpage = str_replace("charset=UTF-8","charset=ISO-8859-1", $utfpage);
+$isopage = utf8_decode($utfpage);
+header("Content-Type: text/plain; charset=ISO-8859-1");
+echo $isopage;
+?>
