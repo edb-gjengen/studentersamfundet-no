646b6d576e5038c152dd04ca133d94d1f8b026c4
Jonas Braathen <jonas.braathen@studentersamfundet.no>
2012-10-10 21:22:33 +0200

    adds google calendar support

---
+++ wp/wp-content/themes/neuf-old-style/program-ical-regen.php
@@ -0,0 +1,65 @@
+<?php
+require_once('../../../wp-load.php');
+require_once('iCalcreator.class.php');
+//http://kigkonsult.se/iCalcreator/
+
+$config = array(
+	"unique_id" => "studentersamfundet.no",
+	"directory" => "cal",
+	"filename"  => "studentersamfundet.ics"
+);
+$v = new vcalendar($config);
+$tz = "Europe/Oslo";
+
+$v->setProperty("method", "PUBLISH");
+$v->setProperty("x-wr-calname", "Studentersamfundet");
+$v->setProperty("x-wr-caldesc", "Program for Det Norske Studentersamfund / Chateau Neuf");
+$v->setProperty("x-wr-timezone", $tz);
+$xprops = array("X-LIC-LOCATION" => $tz);
+iCalUtilityFunctions::createTimezone($v, $tz, $xprops);
+
+$meta_query = array(
+	'key'		=> '_neuf_events_starttime',
+	'value'	=> array(date('U', strtotime('-6 months')), date('U', strtotime('+1 year'))),
+	'type'		=> 'numeric',
+	'compare'	=> 'BETWEEN'
+);
+
+$args = array(
+	'post_type'	=> 'event',
+	'meta_query'	=> array($meta_query),
+	'posts_per_page' => -1,
+	'orderby'	=> 'meta_value_num',
+	'meta_key'	=> '_neuf_events_starttime',
+	'order'	=> 'ASC'
+);
+
+$events = new WP_Query($args);
+
+if ($events->have_posts()) {
+	while ($events->have_posts()) {
+		$events->the_post(); 
+
+		$neuf_events_starttime = get_post_meta($post->ID, '_neuf_events_starttime', true);
+		$neuf_events_endtime = get_post_meta($post->ID, '_neuf_events_endtime', true);
+		$neuf_events_venue = get_post_meta($post->ID, '_neuf_events_venue', true);
+
+		if (!is_numeric($neuf_events_endtime)) {
+			$neuf_events_endtime = $neuf_events_starttime + 7200; //no end time? assume 2 hours
+		}
+
+		$dtstart = date_i18n('Ymd\THis', (int) $neuf_events_starttime);
+		$dtend = date_i18n('Ymd\THis', (int) $neuf_events_endtime);
+		$description = strip_tags(get_the_content());
+
+		$vevent = & $v->newComponent("vevent");
+		$vevent->setProperty("dtstart",		$dtstart);
+		$vevent->setProperty("dtend",		$dtend);
+		$vevent->setProperty("location",		$neuf_events_venue);
+		$vevent->setProperty("summary",		get_the_title());
+		$vevent->setProperty("description",	$description);
+	}
+}
+
+$v->saveCalendar();
+?>
