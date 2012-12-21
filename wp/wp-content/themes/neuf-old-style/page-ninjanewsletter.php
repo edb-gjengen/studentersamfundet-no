6f4305fa68fff75e23b3c11bfc30954e047f7a54
Nikolai Kristiansen <nikolaik@gmail.com>
2012-12-21 13:47:35 +0100

    Adds variable number of ?articles

--- wp/wp-content/themes/neuf-old-style/page-ninjanewsletter.php
+++ wp/wp-content/themes/neuf-old-style/page-ninjanewsletter.php
@@ -2,8 +2,8 @@
 /**
  * Template Name: Ninjanewsletter
  */
-
-$utfpage = file_get_contents("http://studentersamfundet.no/nyhetsbrev");
+$params = http_build_query($_GET);
+$utfpage = file_get_contents(get_home_url()."/nyhetsbrev/?$params");
 $utfpage = str_replace("charset=UTF-8","charset=ISO-8859-1", $utfpage);
 $isopage = utf8_decode($utfpage);
 header("Content-Type: text/plain; charset=ISO-8859-1");
