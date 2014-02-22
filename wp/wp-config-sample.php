6fc552b4fac650870e911acc4c38b2f3873f1805
Nikolai Kristiansen <nikolaik@gmail.com>
2014-02-22 13:34:54 +0100

    Adds (nanananana) batcache, updates plugin

--- wp/wp-config-sample.php
+++ wp/wp-config-sample.php
@@ -14,6 +14,8 @@
  * @package WordPress
  */
 
+define('WP_CACHE', false);
+
 // ** MySQL-innstillinger - Dette får du fra din nettjener ** //
 /** Navnet på WordPress-databasen */
 define('DB_NAME', 'database_name_here');
