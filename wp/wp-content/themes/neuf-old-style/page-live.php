bf94c371ff3562e307b803ca06670f291f91f06a
Nikolai R Kristiansen <nikolaik@gmail.com>
2015-04-14 20:21:02 +0200

    Live changes

--- wp/wp-content/themes/neuf-old-style/page-live.php
+++ wp/wp-content/themes/neuf-old-style/page-live.php
@@ -8,35 +8,19 @@
 
         <div id="flowplayer" class="grid_8" style="background-color:black; color:white;margin-bottom:20px;">
             <!-- Stream URL -->
-            <div id="player" style="width:900px;height:576px;"></div> 
+            <a href="http://porter.streaming.neuf.no:8800/live/" style="width:792px;height:432px;display:block;" id="player"></a>
             <script src="<?php bloginfo('stylesheet_directory'); ?>/flowplayer-3.2.6.min.js"></script>
             <script type="text/javascript"> 
             $(function(){
                 flowplayer(
                     "player",
-                    "<?php bloginfo('stylesheet_directory');?>/js/flowplayer/flowplayer-3.2.15.swf",
-                    {
-                        clip: {
-                            url: 'genfors_nov13',
-                            live: true,
-                            // configure clip to use neuf as our provider, it uses our rtmp plugin
-                            provider: 'neuf'
-                        },
-                     plugins: {
-                        // here is our rtmp plugin configuration
-                        neuf: {
-                            url: "<?php bloginfo('stylesheet_directory');?>/js/flowplayer/flowplayer.rtmp-3.2.12.swf",
-                     
-                            // netConnectionUrl defines where the streams are found
-                            netConnectionUrl: 'rtmp://gemini.neuf.no/live'
-                        }
-                    }
-                });
+                    "<?php bloginfo('stylesheet_directory');?>/flowplayer-3.2.7.swf"
+                );
             });
             </script> 
         </div> 
         <div id="twitter-widget" class="grid_4">
-		<!-- TODO -->
-	</div>
+                <!-- TODO -->
+        </div>
 </div>
 <?php get_footer(); ?>
