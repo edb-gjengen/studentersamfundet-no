9f819b672d85f40eef9e96d444093d010a6f566b
Nikolai Kristiansen <nikolaik@gmail.com>
2016-02-03 21:20:10 +0100

    Style association page

--- wp/wp-content/themes/dns2015/page-templates/page-associations.php
+++ wp/wp-content/themes/dns2015/page-templates/page-associations.php
@@ -12,18 +12,20 @@ $associations = new WP_Query( $args );
 
 <?php get_header(); ?>
 <div id="content">
-	<div class="container">
+	<div class="associations">
         <article <?php neuf_post_class(); ?>>
             <h1 class="entry-title"><?php the_title(); ?></h1>
-            <ul class="associations-list">
+            <ul class="associations--list">
             <?php the_content(); ?>
             <?php if( $associations->have_posts() ):  while( $associations->have_posts() ): $associations->the_post(); ?>
-                <li class="association">
-                    <h2><a href="<?php the_permalink(); ?>"> <?php echo $post->post_title; ?></a></h2>
-                    <?php if( has_post_thumbnail() ): ?>
-                        <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'association-thumb' ); ?></a>
-                    <?php endif; ?>
-                    <?php the_excerpt(); ?>
+                <li class="associations--list--item">
+                    <div class="associations--list--item--inner">
+                        <h2 class="associations--list--item--title"><a href="<?php the_permalink(); ?>"> <?php echo $post->post_title; ?></a></h2>
+                        <?php if( has_post_thumbnail() ): ?>
+                            <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'association-thumb' ); ?></a>
+                        <?php endif; ?>
+                        <?php the_excerpt(); ?>
+                    </div>
                 </li>
             <?php endwhile; endif; ?>
             </ul>
