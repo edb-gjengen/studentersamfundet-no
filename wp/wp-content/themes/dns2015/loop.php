92e09ba6ee1c743a5b8a24ca17ff5f4772554390
Nikolai Kristiansen <nikolaik@gmail.com>
2016-02-14 16:34:25 +0100

    feat(ux): Make post title a link

--- wp/wp-content/themes/dns2015/loop.php
+++ wp/wp-content/themes/dns2015/loop.php
@@ -2,7 +2,7 @@
 
     <article <?php neuf_post_class('slim'); ?>>
         <div class="entry-start">
-            <h3 class="entry-title"><?php the_title(); ?></h3>
+            <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <?php if (get_post_type() == 'event'): ?>
                 <span class="event-date"><?php echo ucfirst( date_i18n( 'l j. F Y' , (int) $post->neuf_events_starttime ) ); ?></span>
             <?php else: ?>
