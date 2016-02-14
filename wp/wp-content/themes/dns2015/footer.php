71eeca2150735bced57d6415411369efabfa88b0
Nikolai Kristiansen <nikolaik@gmail.com>
2016-02-14 16:14:45 +0100

    feat(browsercompat): Add outdated browser warning snippet

--- wp/wp-content/themes/dns2015/footer.php
+++ wp/wp-content/themes/dns2015/footer.php
@@ -19,6 +19,16 @@
 </footer> <!-- #site-footer -->
 
 <?php wp_footer(); ?>
-
+<!-- https://www.browser-update.org/ -->
+<script>
+    var $buoop = {c:2};
+    function $buo_f(){
+        var e = document.createElement("script");
+        e.src = "//browser-update.org/update.min.js";
+        document.body.appendChild(e);
+    }
+    try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+    catch(e){window.attachEvent("onload", $buo_f)}
+</script>
 </body>
 </html>
