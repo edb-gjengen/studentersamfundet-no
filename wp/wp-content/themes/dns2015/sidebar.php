ca6b391105ef21df269a6949b28f4f0192370e31
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-09-22 20:49:16 +0200

    association: sort and exclude current

--- wp/wp-content/themes/dns2015/sidebar.php
+++ wp/wp-content/themes/dns2015/sidebar.php
@@ -13,18 +13,23 @@
     </div>
 
     <?php if(get_post_type() == 'association'):
-    $associations = new WP_Query( array('post_type' => 'association', 'posts_per_page' => 50, ) );
-    if( $associations->have_posts() ):
-        ?>
-        <br><h5>Andre foreninger</h5>
-        <ul>
+        $args = array(
+            'post_type' => 'association',
+            'posts_per_page' => 50,
+            'post__not_in' => array($post->ID),
+            'order_by' => 'title');
+        $associations = new WP_Query( $args );
+        if( $associations->have_posts() ):
+            ?>
+            <br><h5>Andre foreninger</h5>
+            <ul>
+            <?php
+            while( $associations->have_posts() ): $associations->the_post(); ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php endwhile; ?>
+            </ul>
         <?php
-        while( $associations->have_posts() ): $associations->the_post(); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php endwhile; ?>
-        </ul>
-    <?php
-    endif;
+        endif;
     endif; ?>
     <?php dynamic_sidebar('Sidebar'); ?>
 </aside>
