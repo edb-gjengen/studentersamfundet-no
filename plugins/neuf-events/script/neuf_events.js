f01c2978d295c141e617a7edddfcf556a89977e6
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-08-10 20:56:36 +0200

    Try fixing page preview

--- plugins/neuf-events/script/neuf_events.js
+++ plugins/neuf-events/script/neuf_events.js
@@ -1,21 +1,25 @@
 jQuery(document).ready(function($){
-	/* Datetimepicker */
-	$('.datepicker').datetimepicker({
-		currentText: 'Nå',
-		closeText: 'Ok',
-		hourText: 'Time',
-		dateFormat: 'yy-mm-dd',
-		timeFormat: 'HH:mm',
-		minuteText: 'Minutt',
-		timeText: 'Tid',
-		firstDay: 1,
-		monthNames: ['Januar','Februar','Mars','April','Mai','Juni','Juli','August','September','Oktober','November','Desember'],
-		dayNames: ['søndag','mandag','tirsdag','onsdag','torsdag','fredag','lørdag'],
-		dayNamesShort: ['søn','man','tir','ons','tor','fre','lør'],
-		dayNamesMin: ['sø','ma','ti','on','to','fr','lø']
+    /* Datetimepicker */
+    $('.datepicker').datetimepicker({
+        currentText: 'Nå',
+        closeText: 'Ok',
+        hourText: 'Time',
+        dateFormat: 'yy-mm-dd',
+        timeFormat: 'HH:mm',
+        minuteText: 'Minutt',
+        timeText: 'Tid',
+        firstDay: 1,
+        monthNames: ['Januar','Februar','Mars','April','Mai','Juni','Juli','August','September','Oktober','November','Desember'],
+        dayNames: ['søndag','mandag','tirsdag','onsdag','torsdag','fredag','lørdag'],
+        dayNamesShort: ['søn','man','tir','ons','tor','fre','lør'],
+        dayNamesMin: ['sø','ma','ti','on','to','fr','lø']
     });
 
     /* Validation rules */
+    if( $('#post_type').val() !== 'event' ) {
+        return;
+    }
+
     $("#post").validate({
         rules: {
             neuf_events_start_time: "required"
