e5c5f8ead7589a071ca3c63d4529d469b1e7faf4
Nikolai R Kristiansen <nikolai@lucida.no>
2017-09-13 20:22:46 +0200

    More fonts

--- dns2015/header.php
+++ dns2015/header.php
@@ -7,7 +7,7 @@
     <?php neuf_doctitle(); ?>
     <?php get_template_part('favicons'); ?>
     <link rel="alternate" type="application/rss+xml" href="https://studentersamfundet.no/syndikering/kommende-program/" title="Det Norske Studentersamfund (kommende program)"/>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,900,300,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto:400,700,900,300,400i,700i' rel='stylesheet' type='text/css'>
 
     <meta property="fb:app_id" content="220213643760"/>
     <?php wp_head(); ?>
